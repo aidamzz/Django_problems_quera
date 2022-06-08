2368b309a61dbd04f3de61de0f729d4d997e92dd
aidamzz <aidamohagheghzade@gmail.com>
2022-06-08 19:00:15 +0430

    درست نیست

--- 129727/migration/blog/models.py
+++ 129727/migration/blog/models.py
@@ -1,9 +1,24 @@
+
 from django.db import models
+from pytz import timezone
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    status = models.BooleanField(default=True)
 
 
 class Article(models.Model):
-    author = models.CharField(max_length=50)
+    l =(("d", "draft"), ("p", "publish"))
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     category = models.CharField(max_length=50)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    updated = models.DateTimeField(default=created)
+    published = models.DateTimeField(default=timezone.now)
+    status = models.CharField(default='d', choices= l)
+
+
+
